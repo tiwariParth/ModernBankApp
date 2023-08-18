85d5dbb0adbb52f3697fcf36a075d100dbe3be32
Bingolast <243mjparth@gmail.com>
2023-08-18 07:25:54 +0530

    hero Section final commit

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { arrowUp } from "../assets";
 const GetStarted = () => {
   return (
     <div
-      className={`${styles.flexCenter} w-[148px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      className={`${styles.flexCenter} w-[128px] h-[120px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
     >
       <div
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
