ce84f9b4be15cd6480cc666fafc47492a4396b10
Bingolast <243mjparth@gmail.com>
2023-08-19 07:56:32 +0530

    Initial commit

--- src/components/Buisness.jsx
+++ src/components/Buisness.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const Buisness = () => {
-  return (
-    <div>Buisness</div>
-  )
-}
+  return <div>Buisness</div>;
+};
 
-export default Buisness
+export default Buisness;
