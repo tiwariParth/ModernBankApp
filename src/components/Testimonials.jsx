39a7e8c46930d04f8c119f94042718272fa07176
Bingolast <243mjparth@gmail.com>
2023-08-20 14:10:14 +0530

    Updated FeedbackCard component to include quotes, name, title, and img props. Also updated Testimonials component to render FeedbackCards.

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { feedback } from "../constants";
 import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
+import { card } from "../assets";
 
 const Testimonials = () => {
   return (
@@ -16,11 +17,18 @@ const Testimonials = () => {
           What people are <br className="sm:block hidden" /> saying about us
         </h1>
         <div className="w-full md:mt-0 mt-6">
-          <p>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
             Everything you need to accept card payments and grow your business
             anywhere on the planet.
           </p>
         </div>
+      </div>
+      <div
+        className={`flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]`}
+      >
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
       </div>
     </section>
   );
