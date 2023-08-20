b49226d9eeaaf19f4e75b68834c92d36e6d5e60b
Bingolast <243mjparth@gmail.com>
2023-08-20 11:13:09 +0530

    final commit

--- src/components/Buisness.jsx
+++ src/components/Buisness.jsx
@@ -9,8 +9,18 @@ const FeatureCard = ({ icon, title, content, index }) => (
       index !== features.length - 1 ? "mb-6" : "mb-0"
     } feature-card`}
   >
-    <div className={`w-[64px]`}>
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
       <img src={icon} alt="icons" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex flex-1 flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
     </div>
   </div>
 );
