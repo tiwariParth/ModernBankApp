91b6ea2d9fd523259524ff73d0ffc3074f02af1e
Bingolast <243mjparth@gmail.com>
2023-08-20 08:50:34 +0530

    Buisness component final commit

--- src/components/Buisness.jsx
+++ src/components/Buisness.jsx
@@ -4,7 +4,22 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const Buisness = () => {
-  return <div>Buisness</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we'll handle
+          the money
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5s`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+    </section>
+  );
 };
 
 export default Buisness;
