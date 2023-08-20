19f9c2aa3083703e5923e0c99fb8107dd03399d4
Bingolast <243mjparth@gmail.com>
2023-08-20 16:27:57 +0530

    Added footer with logo, company description and links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,43 @@ import { footerLinks, socialMedia } from "../constants";
 const Footer = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-      <div
-        className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}
-      >
-        
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className="flex-1 flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="hooBank"
+            className="w-[266px] h-[72px] object-contain"
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px] `}>
+            A new way to make the payments easy, reliable and secure.
+          </p>
+        </div>
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerLinks) => (
+            <div
+              key={footerLinks.key}
+              className="flex flex-col ss:my-0 my-4 min-x-[150px]"
+            >
+              <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+                {footerLinks.title}
+              </h4>
+              <ul className="list-none mt-4">
+                {footerLinks.links.map((link, index) => (
+                  <li
+                    key={link.name}
+                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                      index !== footerLinks.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
+      
     </section>
   );
 };
