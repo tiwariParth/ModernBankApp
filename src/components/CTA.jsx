43b8e716305b73a5ea7b0e2a1c85f480f968b2c0
Bingolast <243mjparth@gmail.com>
2023-08-20 15:01:38 +0530

    Updated CTA component with styles and button.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div>
+        <h2>Let’s try our service now!</h2>
+        <p>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div>
+        <Button />
+      </div>
+    </section>
+  );
+};
 
-export default CTA
+export default CTA;
