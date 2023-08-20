fe32460787e64b17862b53081f2a8feafb9c0fae
Bingolast <243mjparth@gmail.com>
2023-08-20 17:02:23 +0530

    Changed `Billing` component to use `object-cover` for the background image.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,7 +9,7 @@ const Billing = () => {
         <img
           src={bill}
           alt="billing"
-          className="w-[100%] h-[100vh] relative z-[5]"
+          className="w-[100%] h-[100%] relative z-[5] object-cover"
         />
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
@@ -30,7 +30,7 @@ const Billing = () => {
             alt="apple_store"
             className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
           />
-           <img
+          <img
             src={google}
             alt="google_store"
             className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
