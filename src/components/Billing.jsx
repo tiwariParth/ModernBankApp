b722de4868e83329ff18b8d6c4c9b89f6e150713
Bingolast <243mjparth@gmail.com>
2023-08-20 12:16:15 +0530

    final commit

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -24,7 +24,18 @@ const Billing = () => {
           aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
           placerat.
         </p>
-        
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+           <img
+            src={google}
+            alt="google_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+        </div>
       </div>
     </section>
   );
