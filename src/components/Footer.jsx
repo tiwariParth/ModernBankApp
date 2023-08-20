39a1516972f6cb19e8599c4e851ecd8ee1c8d82c
Bingolast <243mjparth@gmail.com>
2023-08-20 16:39:14 +0530

    Added social media icons to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,23 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45] ">
+        <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+          2023 HooBank. All Rights Reserved.
+        </p>
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+              src={social.icon}
+              alt={social.id}
+              key={social.id}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
